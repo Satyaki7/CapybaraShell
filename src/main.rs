52e29aa69e3fde9d5140966972fa2a713e6bf17b
Satyaki7 <dsatyaki956@gmail.com>
2026-05-09 18:02:36 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 use std::env;
 use std::path::Path;
 use std::fs;
 use std::os::unix::fs::PermissionsExt;
+use std::process::Command;
 
 fn is_builtin(cmd: &str) -> bool {
-    matches!(cmd, "exit" | "echo" | "type")
+   return matches!(cmd, "exit" | "echo" | "type")
 }
 
+fn is_executable(cmd: &str) -> Option<String> {
+    let path_var = env::var("PATH").unwrap_or_default();
+
+    for dir in path_var.split(':') {
+        let full_path = Path::new(dir).join(cmd);
+        if full_path.exists() {
+            if let Ok(metadata) = fs::metadata(&full_path) {
+                let perms = metadata.permissions();
+                if perms.mode() & 0o111 != 0 {
+                    return Some(full_path.to_string_lossy().to_string());
+                }
+            }
+        }
+    }
+    None
+}
 
 fn main() {
     loop{
@@ -30,36 +46,24 @@ fn main() {
                 if is_builtin(cmd) {
                     println!("{} is a shell builtin ", cmd);
                     continue;
-                }
-                let path_var = env::var("PATH").unwrap_or_default();
-                let mut found = false;
-
-                //splitting PATH variable and checking each directory.
-                 for dir in path_var.split(':') {
-                    let full_path = Path::new(dir).join(cmd);
-
-                    // check file exists
-                    if full_path.exists() {
-
-                        // check executable permission by reading the metadata and checking the permissions.
-                        if let Ok(metadata) = fs::metadata(&full_path) {
-                            let perms = metadata.permissions();
-
-                            if perms.mode() & 0o111 != 0 {
-                                println!("{} is {}", cmd, full_path.display());
-                                found = true;
-                                break;
-                            }
-                        }
-                    }
-                }
-                if !found {
+                }else if let Some(path) = is_executable(cmd) {
+                    println!("{cmd} is {path}");
+                } else {
                     println!("{cmd}: not found");
                 }
             },
 
             ["type", args @ ..] => println!("{}: not found", args[0]),
-            _ => println!("{}: command not found", command.trim()),
+            [cmd, args @ ..] => {
+                if let Some(path) = is_executable(cmd) {
+                    let _result = Command::new(path)
+                        .args(args)
+                        .status()
+                        .expect("{cmd}: command execution failed");
+                } else {
+                    println!("{}: command not found", cmd);
+                }
+            }
         }
     }
 }
