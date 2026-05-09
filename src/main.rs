b0f0e9d415fcf8de6c563731cc8f3e82a8cf3fac
Satyaki7 <dsatyaki956@gmail.com>
2026-05-09 17:05:15 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::env;
+use std::path::Path;
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
+
+fn is_builtin(cmd: &str) -> bool {
+    matches!(cmd, "exit" | "echo" | "type")
+}
+
 
 fn main() {
     loop{
@@ -8,11 +17,46 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
         
-        match command.trim().split_whitespace().collect::<Vec<&str>>().as_slice(){
+        match command
+        .trim()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .as_slice(){
+
             [] => continue,
             ["exit"] => break,
             ["echo", args @ ..] => println!("{}", args.join(" ")),
-            ["type", args @ ("exit"|"echo"|"type")] => println!("{} is a shell builtin", args),
+            ["type", cmd] => {
+                if is_builtin(cmd) {
+                    println!("{}: shell built-in command", cmd);
+                }
+                let path_var = env::var("PATH").unwrap_or_default();
+                let mut found = false;
+
+                //splitting PATH variable and checking each directory.
+                 for dir in path_var.split(':') {
+                    let full_path = Path::new(dir).join(cmd);
+
+                    // check file exists
+                    if full_path.exists() {
+
+                        // check executable permission by reading the metadata and checking the permissions.
+                        if let Ok(metadata) = fs::metadata(&full_path) {
+                            let perms = metadata.permissions();
+
+                            if perms.mode() & 0o111 != 0 {
+                                println!("{} is {}", cmd, full_path.display());
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if !found {
+                    println!("{cmd}: not found");
+                }
+            },
+
             ["type", args @ ..] => println!("{}: not found", args[0]),
             _ => println!("{}: command not found", command.trim()),
         }
